60e21a921b8d0df3aad98b7448dbe3410a5d8664
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-13 01:47:27 -0500

    Creacion de Navigation Drawer y Bottom Navigation Bar

---
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/Vendedor/MainActivityVendedor.kt
@@ -0,0 +1,16 @@
+package com.proyecto.vendex_proyecto_final.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.proyecto.vendex_proyecto_final.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_vendedor)
+
+    }
+}
