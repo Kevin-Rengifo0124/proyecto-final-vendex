360efad416b943a6c3bf1b7b011a5448f0410ed2
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-14 00:28:22 -0500

    Se crea el RegistroVendedorActivity y se configura los strings del registro vendedor

---
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/Vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,13 @@
+package com.proyecto.vendex_proyecto_final.Vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.proyecto.vendex_proyecto_final.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+
+    }
+}
