b103de0b432453721b588a6f03aabe3106919d0d
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-15 10:47:23 -0500

    Se crea y se configura los string para el Navigation Drawer de tipo cliente

--- app/src/main/java/com/proyecto/vendex_proyecto_final/Cliente/MainActivityCliente.kt
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/Cliente/MainActivityCliente.kt
@@ -1,9 +1,10 @@
-package com.proyecto.vendex_proyecto_final
+package com.proyecto.vendex_proyecto_final.Cliente
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.proyecto.vendex_proyecto_final.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
