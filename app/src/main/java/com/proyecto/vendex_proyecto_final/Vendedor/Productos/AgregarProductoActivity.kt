e79d79c51f3863ab4127dbd09550e0294917e787
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-16 02:57:44 -0500

    Se crea la vista agregar producto

---
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/Vendedor/Productos/AgregarProductoActivity.kt
@@ -0,0 +1,13 @@
+package com.proyecto.vendex_proyecto_final.Vendedor.Productos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.proyecto.vendex_proyecto_final.R
+
+class AgregarProductoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_producto)
+
+    }
+}
