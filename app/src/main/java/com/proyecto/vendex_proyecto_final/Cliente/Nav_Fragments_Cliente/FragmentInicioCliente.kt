44af320b57ce1e2d7d4f505ac991f7cd5e48e0ba
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-15 19:43:53 -0500

    Se realiza la configuración completa del FragmentInicioCliente

--- app/src/main/java/com/proyecto/vendex_proyecto_final/Cliente/Nav_Fragments_Cliente/FragmentInicioCliente.kt
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/Cliente/Nav_Fragments_Cliente/FragmentInicioCliente.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.proyecto.vendex_proyecto_final.Cliente.Bottom_Nav_Fragments_Cliente.FragmentMisOrdenesCliente
+import com.proyecto.vendex_proyecto_final.Cliente.Bottom_Nav_Fragments_Cliente.FragmentTiendaCliente
 import com.proyecto.vendex_proyecto_final.R
+import com.proyecto.vendex_proyecto_final.databinding.FragmentInicioClienteBinding
 
 
 class FragmentInicioCliente : Fragment() {
 
+    private lateinit var binding: FragmentInicioClienteBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_cliente, container, false)
+        binding = FragmentInicioClienteBinding.inflate(inflater, container, false)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.opcion_tienda_cliente -> {
+                    replaceFragment(FragmentTiendaCliente())
+                }
+
+                R.id.opcion_mis_ordenes_cliente -> {
+                    replaceFragment(FragmentMisOrdenesCliente())
+                }
+
+            }
+            true
+        }
+        replaceFragment(FragmentTiendaCliente())
+        binding.bottomNavigation.selectedItemId = R.id.opcion_tienda_cliente
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction().replace(R.id.bottomFragment, fragment).commit()
     }
 
 
