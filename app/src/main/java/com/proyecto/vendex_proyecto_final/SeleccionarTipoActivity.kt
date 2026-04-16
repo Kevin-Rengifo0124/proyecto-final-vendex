66a136c89d2a38229d81fc60bc2e03394616d619
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2026-04-16 00:16:06 -0500

    Se crea la vista tipo de usuario

---
+++ app/src/main/java/com/proyecto/vendex_proyecto_final/SeleccionarTipoActivity.kt
@@ -0,0 +1,13 @@
+package com.proyecto.vendex_proyecto_final
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SeleccionarTipoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccionar_tipo)
+
+    }
+}
